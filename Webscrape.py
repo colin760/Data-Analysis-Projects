baf914af645d36fdd4e9d916114cdf43f13e1c8c
colin760 <68519303+colin760@users.noreply.github.com>
2020-08-11 16:21:13 -0400

    Update Webscrape.py

--- Webscrape.py
+++ Webscrape.py
@@ -3,28 +3,39 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as urec
 from lxml import html
 
+#Uses the url and requests access to the site
 url = "https://www.microcenter.com/category/4294966937/Video-Cards"
 web = urec(url)
+#Reads the website data in 
 page_html = web.read()
 web.close()
+#Creates a beautiful soup object to parse the data in the website
 soup = bs(page_html, "html.parser")
 #print(soup.prettify())
 
+#Finds all the details of the products on the page
 details = soup.findAll('div', class_='details')
 #print(len(details))
 
+
 detail = details[0]
+#Finds the price of one product
 detail.find("div", {'class':'price'}).text.strip()
+
+#Finds the name of one product 
 normal = detail.find("div", {'class': 'normal'})
 normal.a['data-name']
 
+#Finds the availability of one product 
 detail.find('p', {'class': 'availabilityTrunc'}).text
 
+#Creats a file to write in the values parsed from the website 
 filename = "video-cards.csv"
 headers = "Price, Name, Availability\n"
 f = open(filename, "w")
 f.write(headers)
 
+#Creates a loop to go through every product on the page 
 for detail in details:
     price = detail.find("div", {'class':'price'}).text.strip()
     
@@ -38,4 +49,6 @@ for detail in details:
     
     f.write(price + "," +name + "," +availability + "\n")
 f.close()
+
+
 
